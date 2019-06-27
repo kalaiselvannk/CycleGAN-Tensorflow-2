59789f23097c61c6e1ee280bf9b94c9cd21b1565
kalaiselvannk <kalaiselvannk@rediffmail.com>
2019-06-27 07:21:45 -0700

    removed verbose update

--- download_dataset.sh
+++ download_dataset.sh
@@ -4,7 +4,7 @@ export KAGGLE_KEY="4f9449b9ef18af2a0715b1c0e6472998"
 kaggle datasets download -d thedagger/pokemon-generation-one
 kaggle datasets download -d jessicali9530/stanford-dogs-dataset
 unzip stanford-dogs-dataset.zip -d ./dataset_dogs
-tar -xvf ./dataset_dogs/images.tar
+tar -xf ./dataset_dogs/images.tar
 rm -r dataset_dogs
 unzip pokemon-generation-one.zip -d ./dataset_pokemon
 
